fc2936afa00681626677ac1cafc998846c4e0c7f
JirapunJunruk <jirapun0907@gmail.com>
2024-04-07 13:12:06 +0700

    update
    update

--- src/content/Resume.js
+++ src/content/Resume.js
@@ -12,8 +12,8 @@ class Resume extends Component{
             <div className="GG">
                 {/* <img src="https://scontent.fbkk12-2.fna.fbcdn.net/v/t39.30808-6/287170483_3507281356224800_5802208541541541382_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGYFpUuyySf9XuF39x3Tk_ZfqqcTppY0NV-qpxOmljQ1fFzjOHxIiq3iQnLb7I6IT2-IxAmj6jjHXb1zQhVvIBD&_nc_ohc=S6Kw2WEVyp4AX84Jpk9&_nc_ht=scontent.fbkk12-2.fna&oh=00_AfBWE7eFsRi1iYdgSg46b-SkIvCEb-YZenZPzo_BTkAOwg&oe=65396B5E"
                 alt="profile" className="resume1"/> */}
-                <img src={port} className="resume" alt=""/> <br/>
-                <img src={tran1} className="resume" alt=""/> <br/>
+                <img src={port} className="resume" alt=""/>
+                <img src={tran1} className="resume" alt=""/> 
                 <img src={tran2} className="resume" alt=""/>
             </div>
 
